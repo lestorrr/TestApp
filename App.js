e0264ef4b57e8015f6b3484460fe21af40db966e
lestorrr <meiravalderama@gmail.com>
2024-02-13 07:40:41 +0800

    .

---
+++ App.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
+import Slider from '@react-native-community/slider';
+
+const UltrasonicScreen = ({ sensorData }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.sensorContainer}>
+        <SensorItem label="Chair 1" value={sensorData.chair1} />
+        <SensorItem label="Chair 2" value={sensorData.chair2} />
+        <SensorItem label="Chair 3" value={sensorData.chair3} />
+        <SensorItem label="Chair 4" value={sensorData.chair4} />
+        <SensorItem label="Chair 5" value={sensorData.chair5} />
+        <SensorItem label="Chair 6" value={sensorData.chair6} />
+      </View>
+    </View>
+  );
+};
+
+const SensorItem = ({ label, value }) => {
+  const detectedValue = value === 'No person detected' ? value : 'Person detected';
+  const textColor = value === 'No person detected' ? 'green' : 'red';
+
+  return (
+    <View style={styles.sensorItem}>
+      <Text style={[styles.sensorLabel, { color: textColor }]}>{label}</Text>
+      <Text style={[styles.sensorValue, { color: textColor }]}>{detectedValue}</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const [sensorData, setSensorData] = useState({
+    chair1: 'No person detected',
+    chair2: 'No person detected',
+    chair3: 'No person detected',
+    chair4: 'No person detected',
+    chair5: 'No person detected',
+    chair6: 'No person detected',
+  });
+  const [personDetectedCount, setPersonDetectedCount] = useState(0);
+  const [noPersonDetectedCount, setNoPersonDetectedCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(fetchData, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('ESP266_IP_ADDRESS/sensors');
+      setSensorData(response.data);
+
+      const detectedCount = Object.values(response.data).filter(value => value !== 'No person detected').length;
+      const notDetectedCount = Object.values(response.data).filter(value => value === 'No person detected').length;
+
+      setPersonDetectedCount(detectedCount);
+      setNoPersonDetectedCount(notDetectedCount);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Vacant Seat Monitoring</Text>
+      <View style={styles.tabContainer}>
+
+      </View>
+      <UltrasonicScreen sensorData={sensorData} />
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>Person Detected: {personDetectedCount}</Text>
+        <Text style={styles.countText}>No Person Detected: {noPersonDetectedCount}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+  },
+  sensorContainer: {
+    marginBottom: 20,
+  },
+  sensorItem: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 70,
+    width: '100%',
+    marginBottom: 2,
+  },
+  sensorLabel: {
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 60,
+    width: '100%',
+    marginBottom: 1,
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  sensorValue: {
+    fontSize: 20,
+
+  },
+  countContainer: {
+    marginTop: 10,
+  },
+  countText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
